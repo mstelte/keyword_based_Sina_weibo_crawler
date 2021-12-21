2cdfa992d4fb1bf4393ec8e850ffad5cc65cbfcf
mstelte <marcel.stelte@gmail.com>
2021-12-21 19:25:27 +0100

    Added sina weibo crawler to the notebook

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ start_date = datetime.datetime(2021, 9, 1, 0, 0, 0, 0, datetime.timezone(datetim
 end_date = datetime.datetime(2021, 12, 18, 23, 59, 59, 59, datetime.timezone(datetime.timedelta()))
 
 searches = []
-searches.append(["#英雄联盟##EDG#", "edg_lol_worlds"])
-searches.append(["#斯德哥尔摩major#", "stockholm_csgo_major"])
-searches.append(["#dota2国际邀请赛#", "dota2_international"])
-searches.append(["#2020欧洲杯#", "euro2020"])
-searches.append(["#环法自行车赛#", "tour_de_france_2021"])
-searches.append(["#NBA总决赛#", "nba_final"])
+searches.append(["#PGC2021#", "PGC2021"])
+searches.append(["#斯德哥尔摩major#", "PGLMAJOR"])
+searches.append(["#2021英雄联盟全球总决赛#", "Worlds2021"])
+searches.append(["#短池世锦赛#", "FINAAbuDhabi2021"])
+searches.append(["#2021MLB世界大赛#", "WorldSeries"])
+searches.append(["#F1阿布扎比大奖赛#", "AbuDhabiGP"])
 
 for search in searches:
     crawl_weibo(search[0], start_date, end_date, search[1])
